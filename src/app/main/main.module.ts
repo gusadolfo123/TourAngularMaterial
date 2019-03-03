c552b20d3b89cf6526445499e7ce576f0842d150
gusadolfo123 <gusadolfo123@hotmail.com>
2019-03-03 08:55:11 -0500

    Ajuste Pagina Lugares

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -6,13 +6,23 @@ import {ContactComponent} from './contact/contact.component';
 import {AboutComponent} from './about/about.component';
 import {MainRoutingModule} from './main-routing.module';
 import {SharedModule} from '../shared/shared.module';
-import {MatButtonModule} from '@angular/material';
-import { PromotionComponent } from './index/promotion/promotion.component';
-import { PlacesComponent } from './places/places.component';
-import { DetailComponent } from './places/detail/detail.component';
+import {MatButtonModule, MatCardModule} from '@angular/material';
+import {PromotionComponent} from './index/promotion/promotion.component';
+import {PlacesComponent} from './places/places.component';
+import {DetailComponent} from './places/detail/detail.component';
+import {CardPlaceComponent} from './places/card-place/card-place.component';
 
 @NgModule({
-  declarations: [MainComponent, IndexComponent, ContactComponent, AboutComponent, PromotionComponent, PlacesComponent, DetailComponent],
-  imports: [CommonModule, MainRoutingModule, SharedModule, MatButtonModule],
+  declarations: [
+    MainComponent,
+    IndexComponent,
+    ContactComponent,
+    AboutComponent,
+    PromotionComponent,
+    PlacesComponent,
+    DetailComponent,
+    CardPlaceComponent,
+  ],
+  imports: [CommonModule, MainRoutingModule, SharedModule, MatButtonModule, MatCardModule],
 })
 export class MainModule {}
