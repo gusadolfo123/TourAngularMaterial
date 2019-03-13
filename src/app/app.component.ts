549bc77242619eaf785507f1167301e4717fe55f
gusadolfo123 <gusadolfo123@hotmail.com>
2019-03-12 21:31:52 -0500

    Ajustes Adicionales

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router, NavigationEnd} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'NewTourPage';
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe(evt => {
+      if (!(evt instanceof NavigationEnd)) {
+        return;
+      }
+      window.scrollTo(0, 0);
+    });
+  }
+
+  goTop() {
+    window.scrollTo(0, 0);
+  }
 }
