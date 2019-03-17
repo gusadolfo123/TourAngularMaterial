9c05e2b6f0044ee97c9d5c6f1201f26d611d2daa
gusadolfo123 <gusadolfo123@hotmail.com>
2019-03-17 11:53:53 -0500

    asd

--- src/app/main/index/card-site/card-site.component.ts
+++ src/app/main/index/card-site/card-site.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card-site',
   templateUrl: './card-site.component.html',
-  styleUrls: ['./card-site.component.css']
+  styleUrls: ['./card-site.component.css'],
 })
 export class CardSiteComponent implements OnInit {
+  bogota = 'bogota';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/main/main.routes.ts
+++ src/app/main/main.routes.ts
@@ -2,11 +2,13 @@ import {Routes} from '@angular/router';
 import {IndexComponent} from './index/index.component';
 import {AboutComponent} from './about/about.component';
 import {ContactComponent} from './contact/contact.component';
-import { PlacesComponent } from './places/places.component';
+import {PlacesComponent} from './places/places.component';
+import {DetailComponent} from './places/detail/detail.component';
 
 export const MainRoutes: Routes = [
   {path: '', component: IndexComponent},
   {path: 'places', component: PlacesComponent},
+  {path: 'places/:name', component: DetailComponent},
   {path: 'about', component: AboutComponent},
   {path: 'contact', component: ContactComponent},
 ];
