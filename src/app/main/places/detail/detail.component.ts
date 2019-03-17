a6f9af7169640bfb0f1e17c31f08b750426caa3a
gusadolfo123 <gusadolfo123@hotmail.com>
2019-03-17 17:23:21 -0500

    asdas

--- src/app/main/places/detail/detail.component.ts
+++ src/app/main/places/detail/detail.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
 })
 export class DetailComponent implements OnInit {
+  detail = {};
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  ShowDetailSite(site: any) {
+    this.detail = {
+      Site: 'Museo Nacional',
+      Description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nostrum pariatur eos totam adipisci
+            Consequuntur recusandae eos, repellat optio odio ad, quae nam inventore assumenda at maxime iure sint
+            explicabo`,
+      images: [
+        {url: 'https://picsum.photos/1000/600/?100'},
+        {url: 'https://picsum.photos/1000/600/?120'},
+        {url: 'https://picsum.photos/1000/600/?131'},
+      ],
+    };
   }
-
 }
