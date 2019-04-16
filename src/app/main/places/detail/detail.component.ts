764cacc06d9933c5a8b1532bafc0eab682e64fc6
gusadolfo123 <gusadolfo123@hotmail.com>
2019-04-15 19:38:07 -0500

    Ajustes

--- src/app/main/places/detail/detail.component.ts
+++ src/app/main/places/detail/detail.component.ts
@@ -1,5 +1,5 @@
 // import '../../../../../node_modules/leaflet-routing-machine/dist/leaflet-routing-machine.js';
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, Output, EventEmitter} from '@angular/core';
 // declare let L;
 
 @Component({
@@ -8,13 +8,43 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./detail.component.css'],
 })
 export class DetailComponent implements OnInit {
+  @Output()
+  propagar = new EventEmitter<string>();
+
   detail = {};
   lat: number = 4.624291;
   lng: number = -74.065738;
 
   origin = {lat: 4.624291, lng: -74.065738};
   destination = {lat: 4.601994, lng: -74.072751};
-  markers = [{lat: 4.624291, lng: -74.065738}, {lat: 4.601994, lng: -74.072751}];
+  markers = [
+    {
+      site: 'Parque Nacional',
+      lat: 4.624291,
+      lng: -74.065738,
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nostrum pariatur eos totam adipisci
+    Consequuntur recusandae eos, repellat optio odio ad, quae nam inventore assumenda at maxime iure sint
+    explicabo`,
+      images: [
+        {url: 'https://picsum.photos/1000/600/?100'},
+        {url: 'https://picsum.photos/1000/600/?120'},
+        {url: 'https://picsum.photos/1000/600/?131'},
+      ],
+    },
+    {
+      site: 'Museo del Oro',
+      lat: 4.601994,
+      lng: -74.072751,
+      description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi nostrum pariatur eos totam adipisci
+    Consequuntur recusandae eos, repellat optio odio ad, quae nam inventore assumenda at maxime iure sint
+    explicabo`,
+      images: [
+        {url: 'https://picsum.photos/1000/600/?100'},
+        {url: 'https://picsum.photos/1000/600/?120'},
+        {url: 'https://picsum.photos/1000/600/?131'},
+      ],
+    },
+  ];
 
   constructor() {}
 
